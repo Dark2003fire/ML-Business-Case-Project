2b63dacb153a19c9754cbbdd41d1280ec9c7a86a
Alexandre Zajac <work@alexandrezajac.com>
2021-01-19 11:53:24 +0100

    Type in feature_importance.py

--- web_application/src/pages/modeling.py
+++ web_application/src/pages/modeling.py
@@ -3,12 +3,6 @@ import logging
 
 import streamlit as st
 
-import awesome_streamlit as ast
-from awesome_streamlit.core.services import resources
-
-logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
-
-
 TITLE = "Modeling and Forecasting the Sales"
 
 
